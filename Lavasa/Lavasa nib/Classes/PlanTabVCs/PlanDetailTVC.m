07cc429c0b5ce846b34c6fc57215029c167d0a86
Vivek Sehrawat <vivek.sehrawat@mediaagility.com>
2014-06-27 13:09:36 +0530

    experiment

--- Lavasa/Lavasa nib/Classes/PlanTabVCs/PlanDetailTVC.m
+++ Lavasa/Lavasa nib/Classes/PlanTabVCs/PlanDetailTVC.m
@@ -22,18 +22,10 @@
     }
     return self;
 }
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
-
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
@@ -78,7 +70,6 @@
 }
 - (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    
 }
 - (void)reloadRowsAtIndexPaths:(NSArray *)indexPaths withRowAnimation:(UITableViewRowAnimation)animation
 {
---
+++ Lavasa/sehrawat.h
@@ -0,0 +1,13 @@
+//
+//  sehrawat.h
+//  Lavasa
+//
+//  Created by Vivek Sehrawat on 6/27/14.
+//  Copyright (c) 2014 Mediaagility. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sehrawat : UIViewController
+
+@end
