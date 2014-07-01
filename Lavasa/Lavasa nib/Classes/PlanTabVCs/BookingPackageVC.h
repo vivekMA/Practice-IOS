5e2e939f0193f504c992140729bc636194a05429
Vivek Sehrawat <vivek.sehrawat@mediaagility.com>
2014-07-01 10:36:50 +0530

    Parse bug fixes

---
+++ Lavasa/Lavasa nib/Classes/PlanTabVCs/BookingPackageVC.h
@@ -0,0 +1,15 @@
+//
+//  BookingPackageVC.h
+//  Lavasa
+//
+//  Created by Vivek Yadav on 6/30/14.
+//  Copyright (c) 2014 Mediaagility. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BookingPackageVC : UITableViewController
+
+- (IBAction)AddBtnAction:(id)sender;
+
+@end
