256ab39d2afaaa50741ca2618367de3c371d970d
Vivek Sehrawat <vivek.sehrawat@mediaagility.com>
2014-07-02 19:42:56 +0530

    added sehrwat

---
+++ demo/demo/sehrawat.h
@@ -0,0 +1,13 @@
+//
+//  sehrawat.h
+//  demo
+//
+//  Created by Vivek Sehrawat on 7/2/14.
+//  Copyright (c) 2014 MA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sehrawat : UIViewController
+
+@end
---
+++ demo/demo/sehrawat.m
@@ -0,0 +1,49 @@
+//
+//  sehrawat.m
+//  demo
+//
+//  Created by Vivek Sehrawat on 7/2/14.
+//  Copyright (c) 2014 MA. All rights reserved.
+//
+
+#import "sehrawat.h"
+
+@interface sehrawat ()
+
+@end
+
+@implementation sehrawat
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
